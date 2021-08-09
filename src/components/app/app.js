08d65393a04b8ead9ebd07363187868410626b25
Roman Franko <roma.franko99@gmail.com>
2021-08-10 01:44:42 +0300

    New item page ware created

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {MainPage, CartPage} from '../pages';
+import {MainPage, CartPage, ItemPage} from '../pages';
 import AppHeader from '../app-header';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Background from './food-bg.jpg';
+
 
 const App = () => {
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
+            <Switch>
             <Route path='/' exact component={MainPage}/>
-            <Route path='/cart' component={CartPage}/>
+            <Route path='/cart' exact component={CartPage}/>
+            <Route path = '/:id' exact component={ItemPage}/>
+            </Switch>
         </div>
     )
 }
